76a50c6d05bf935e8dc2f5ad70047e9fdeff3263
João Paulo Mesquita <jp-mesquita@live.com>
2022-10-27 12:19:53 -0300

    chore: finishing the header layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
-import { SearchIcon } from '@heroicons/react/solid'
+import { 
+  GlobeAltIcon,
+  MenuIcon,
+  UserCircleIcon,
+  SearchIcon,
+  UsersIcon
+ } from '@heroicons/react/solid'
 
 
 
@@ -27,8 +33,18 @@ export default function Header(){
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer shrink-0"/>
       </div>
 
-      <div>
-        
+      <div className="flex items-center justify-end space-x-4  text-gray-500">
+        <p className="hidden md:inline cursor-pointer">Become a host</p>
+
+        <GlobeAltIcon 
+          className="h-6 cursor-pointer"
+
+        />
+
+        <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+          <MenuIcon className="h-6 cursor-pointer" />
+          <UserCircleIcon className="h-6 cursor-pointer"/>
+        </div>
       </div>
     </header>
   )
