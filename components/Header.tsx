0cf6f6d76e50308e114fbed81086e7844dc5ac06
João Paulo Mesquita <jp-mesquita@live.com>
2022-10-28 15:40:36 -0300

    feat: add react-date-range and date-fns

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,18 +7,34 @@ import {
   UsersIcon
  } from '@heroicons/react/solid'
 import { ChangeEvent, useState } from "react";
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker, RangeKeyDict, Range } from 'react-date-range'
 
 
 
 export default function Header(){
 
   const [searchInput, setSearchInput] = useState('')
+  const [startDate, setStartDate] = useState(new Date())
+  const [endDate, setEndDate] = useState(new Date())
+
+  const selectionRange ={
+    startDate: startDate,
+    endDate: endDate,
+    key: 'selection'
+  }
 
   function handleChangeInput(event: ChangeEvent<HTMLInputElement>){
     setSearchInput(event.target.value)
     
   }
   
+  function handleSelect(ranges: RangeKeyDict){
+    setStartDate(ranges.selection.startDate)
+    setEndDate(ranges.selection.endDate)
+  }
+
   return(
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       <div className="relative flex items-center h-10 cursor-pointer my-auto">
@@ -57,6 +73,20 @@ export default function Header(){
           <UserCircleIcon className="h-6 cursor-pointer"/>
         </div>
       </div>
+
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto mt-6">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#fd5b61"]}
+            onChange={handleSelect}
+          />
+
+
+        </div>
+      )}
+      
     </header>
   )
 }
