018828596c449ec63d4e0c1709dd0b3242793883
João Paulo Mesquita <jp-mesquita@live.com>
2022-10-28 18:22:27 -0300

    feat: add some buttons in search page

--- pages/search.tsx
+++ pages/search.tsx
@@ -7,7 +7,23 @@ export default function Search(){
       <Header />
 
       <main className="flex">
+      <section className="felx-grow pt-14 px-6">
+          <p className="text-xs">
+            300+ Stays for 5 number of guests
+          </p>
 
+          <h1 className="text-3xl font-semibold mt-2 mb-6">
+            Stays in mars
+          </h1>
+
+          <div className="hidden lg:inline-flex mb-5 gap-3 text-gray-800 whitespace-nowrap">
+            <p className="button">Cancellation Flexibility</p>
+            <p className="button">Type of Place</p>
+            <p className="button">Price</p>
+            <p className="button">Rooms and Beds</p>
+            <p className="button">More filters</p>
+          </div>
+        </section>
       </main>
 
       <Footer />
